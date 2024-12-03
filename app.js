b73f8ba9de04f8b4749c578b4be77a053128ba7f
Matheus Assumpção <matheusassumpcap@gmail.com>
2024-12-03 09:27:58 -0300

    instalação de dependencia cors

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express = require("express")
-const getLivros = require("./rotas/livro")
+const express = require("express");
+const cors = require("cors"); // Importa o CORS
+const getLivros = require("./rotas/livro");
 
-const app = express()
-app.use(express.json())
-const port = 8000
+const app = express();
+app.use(cors()); // Habilita CORS para todas as rotas
+app.use(express.json());
+const port = 8000;
 
-app.use('/livros', getLivros)
+app.use('/livros', getLivros);
 
 app.listen(port, () => {
-    console.log(`Escutando a porta ${port}`)
-})
+    console.log(`Escutando a porta ${port}`);
+});
