028dd9786471de7a8d1627b06c88b0c7473cb808
Matheus Assumpção <matheusassumpcap@gmail.com>
2024-12-05 22:10:09 -0300

    teste

--- rotas/livro.js
+++ rotas/livro.js
@@ -1,6 +1,5 @@
 const { Router } = require("express")
 const { getLivros, getLivro, postLivro, patchLivro, deleteLivro } = require("../controladores/livro")
-const { getFavoritos, postFavorito, deleteFavorito } = require("../controladores/favorito")
 
 const router = Router()
 
@@ -13,11 +12,5 @@ router.post('/', postLivro)
 router.patch('/:id', patchLivro)
 
 router.delete('/:id', deleteLivro)
-
-router.get('/', getFavoritos)
-
-router.post('/', postFavorito)
-
-router.delete('/:id', deleteFavorito)
 
 module.exports = router
